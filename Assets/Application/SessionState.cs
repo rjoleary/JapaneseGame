9d650a3792ce4b3d004f51b402110336924f62d9
Savio <joanes.savio@gmail.com>
2014-05-23 23:47:17 -0400

    Playing around with application side additions

---
+++ Assets/Application/SessionState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Application
+{
+    class SessionState
+    {
+        private static SessionState instance = null;
+        private static readonly object padlock = new object();
+
+        SessionState()
+        {
+            //new game options
+        }
+
+        public static SessionState Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                        instance = new SessionState();
+                    return instance;
+                }
+            }
+        }
+    }
+}
