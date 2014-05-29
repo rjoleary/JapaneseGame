70c04a6e5e2db0fe2ec341c3f78e0e88989b44a8
Savio <joanes.savio@gmail.com>
2014-05-28 22:33:37 -0400

    Made controller more natural

--- Assets/Kenkyuu/Application/Movement.cs
+++ Assets/Kenkyuu/Application/Movement.cs
@@ -3,43 +3,71 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-
-    public float walkSpeed = 8.0F;
-    public float runSpeed = 12.0F;
-    private float speed;
-
-    public float jumpSpeed = 8.0F;
+    public float slow = 8.0F;
+    public float moderate = 10.0F;
+    public float fast = 12.0F;
+    public float jump = 10.0F;
     public float gravity = 20.0F;
-    private Vector3 moveDirection = Vector3.zero;
-    CharacterController controller;
+    public float terminalVelocity = 25.0F;
 
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    private Animator anim;
+    private float currentSpeed;
     void Start(){
-        speed = walkSpeed;
-        controller = GetComponent<CharacterController>();  
+        currentSpeed = slow;
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, moveDirection.y, Input.GetAxis("Vertical") * speed);
-        moveDirection = transform.TransformDirection(moveDirection);
-        tryJump();
-        tryRun();
-        moveDirection.y -= gravity * Time.deltaTime;
+        UpdateDirection();
+        DecideSpeed();
+        UpdateAnimationState();
         controller.Move(moveDirection * Time.deltaTime);
 	}
 
-    private void tryJump() 
+    private void DecideSpeed()
     {
-        if (controller.isGrounded)
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed;
+        if (controller.isGrounded && Input.GetButton("Run"))
+            currentSpeed = fast;
+        else if (Input.GetButton("Run"))
+            currentSpeed = slow;
+        else 
+            currentSpeed = slow;
     }
 
-    private void tryRun()
+    private void UpdateDirection()
     {
-        if (Input.GetButton("Run"))
-            speed = runSpeed;
+        var direction = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        direction.Normalize();
+        moveDirection = new Vector3(direction.x * currentSpeed, moveDirection.y, direction.z * currentSpeed);
+        TryJump();
+    }
+
+    private void UpdateAnimationState()
+    {
+        anim.SetFloat("XZSpeed", IsMovingInXYPlane() ? 0 : currentSpeed);
+        anim.SetFloat("YSpeed", moveDirection.y);
+    }
+
+    private bool IsMovingInXYPlane() {
+        return (Mathf.Abs(Input.GetAxis("Horizontal")) + Mathf.Abs(Input.GetAxis("Vertical"))) == 0;
+    }
+
+    private void TryJump() 
+    {
+        if (controller.isGrounded)
+            moveDirection.y = -1*gravity;
+
+        if (controller.isGrounded && Input.GetButton("Jump"))
+            moveDirection.y = jump;
         else
-            speed = walkSpeed;
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+            moveDirection.y = Mathf.Max(-1 * terminalVelocity, moveDirection.y);
+        }
+            
     }
 }
