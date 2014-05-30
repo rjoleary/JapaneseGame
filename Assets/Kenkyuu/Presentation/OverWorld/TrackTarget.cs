355849099f611c34e0c091acc84a47d8f0841b41
Savio <joanes.savio@gmail.com>
2014-05-29 22:18:05 -0400

    Mostly  thoughtwork

---
+++ Assets/Kenkyuu/Presentation/OverWorld/TrackTarget.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackTarget : MonoBehaviour {
+
+	public float _distance = 5f;
+    public GameObject _target;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPos = _target.transform.position;
+        transform.position = new Vector3(targetPos.x, targetPos.y, targetPos.z + _distance);
+	}
+}
